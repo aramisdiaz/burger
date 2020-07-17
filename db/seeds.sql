b3ddbcde8b45ff46665b68f65a44296e4856624f
Aramis Diaz <aramisdiaz@mail.usf.edu>
2020-07-16 18:04:33 -0700

    Initial commit to github

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE `burgers_db`;
+
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Texas Haystack', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Ahi Tuna', false);
+INSERT INTO `burgers` (burger_name, devoured) VALUES ('Black and Bleu', false);
